1e1617957ae68430a951697e132f77575858c075
Martin PIEGAY <martin.piegay@telecom-st-etienne.fr>
2015-04-20 16:29:25 +0200

    comparaison des methodes de moyenne

--- src/main/java/fr/tse/fi2/hpp/labs/main/MainNonStreaming.java
+++ src/main/java/fr/tse/fi2/hpp/labs/main/MainNonStreaming.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import fr.tse.fi2.hpp.labs.beans.measure.QueryProcessorMeasure;
 import fr.tse.fi2.hpp.labs.dispatcher.LoadFirstDispatcher;
 import fr.tse.fi2.hpp.labs.queries.AbstractQueryProcessor;
+import fr.tse.fi2.hpp.labs.queries.impl.IncrementalAverage;
+import fr.tse.fi2.hpp.labs.queries.impl.NaiveAverage;
 import fr.tse.fi2.hpp.labs.queries.impl.SimpleQuerySumEvent;
 import fr.tse.fi2.hpp.labs.queries.impl.lab1.SumQuery;
 
@@ -37,12 +39,13 @@ public class MainNonStreaming {
 		QueryProcessorMeasure measure = new QueryProcessorMeasure();
 		// Init dispatcher and load everything
 		LoadFirstDispatcher dispatch = new LoadFirstDispatcher(
-				"src/main/resources/data/1000Records.csv");
+				"src/main/resources/data/100k.csv");
 		logger.info("Finished parsing");
 		// Query processors
 		List<AbstractQueryProcessor> processors = new ArrayList<>();
 		// Add you query processor here
-		processors.add(new SumQuery(measure));
+		processors.add(new NaiveAverage(measure));
+		processors.add(new IncrementalAverage(measure));
 		// Register query processors
 		for (AbstractQueryProcessor queryProcessor : processors) {
 			dispatch.registerQueryProcessor(queryProcessor);
