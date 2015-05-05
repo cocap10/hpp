82e5005309f3fdcd5ecb73717a2597a9aa68dbbf
cocap10 <martin.piegay@telecom-st-etienne.fr>
2015-05-05 15:58:32 +0200

    C'est cool

---
+++ src/main/java/fr/tse/fi2/hpp/labs/queries/impl/lab4/RouteMembershipProcessor.java
@@ -0,0 +1,48 @@
+package fr.tse.fi2.hpp.labs.queries.impl.lab4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.tse.fi2.hpp.labs.beans.DebsRecord;
+import fr.tse.fi2.hpp.labs.beans.measure.QueryProcessorMeasure;
+import fr.tse.fi2.hpp.labs.queries.AbstractQueryProcessor;
+
+public class RouteMembershipProcessor extends AbstractQueryProcessor {
+	private List<DebsRecord> recs=null;
+
+	public RouteMembershipProcessor(QueryProcessorMeasure measure) {
+		super(measure);
+		// TODO Auto-generated constructor stub
+		recs= new ArrayList<DebsRecord>();
+	}
+
+	@Override
+	protected void process(DebsRecord record) {
+		// TODO Auto-generated method stub
+		recs.add(record);
+
+	}
+	public int printRoute(int index)
+	{
+		System.out.println(recs.get(index).getPickup_longitude()+" "+recs.get(index).getPickup_latitude()+" "+recs.get(index).getDropoff_longitude()+" "+recs.get(index).getDropoff_latitude()+" "+recs.get(index).getHack_license());
+		return index;
+	}
+	
+	public int lookupForRoute(Float longDep, Float latDep, Float longArr, Float latArr, String HackLic)
+	{
+		int nbTrouve=0;
+		for (int i=0; i<recs.size();i++) {
+			DebsRecord debsRecord = recs.get(i);
+			//System.out.println(i);
+			if (debsRecord.getPickup_longitude()==longDep && debsRecord.getPickup_latitude()==latDep
+					&& debsRecord.getDropoff_longitude()==longArr && debsRecord.getDropoff_latitude()==latArr
+					&& debsRecord.getHack_license()==HackLic)
+			{
+				System.out.println("Trouvé! :-)");
+				nbTrouve++;
+			}
+		}
+		return nbTrouve;
+	}
+
+}
